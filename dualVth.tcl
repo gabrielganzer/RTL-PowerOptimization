a343d8e2fa9c44295ef96845b84ab14bc41c7082
Gabriel Ganzer <gabriel.ganzer@studenti.polito.it>
2020-07-03 15:41:42 +0200

    feat: script now includes a control over the input "savings"

--- dualVth.tcl
+++ dualVth.tcl
@@ -1,108 +1,225 @@
 proc dualVth {args} {
-	parse_proc_arguments -args $args results
-	set lvt $results(-lvt)
-	set constraint $results(-constraint)
-	
-  	suppress_message NED-045
+
+        parse_proc_arguments -args $args results
+        set savings $results(-leakage)
+		
+	suppress_message NED-045
 	suppress_message LNK-041
+	suppress_message LNK-016
 	suppress_message PTE-139
-
-  	#get all data about cells, already sorted by decreasing slack
-	set cells_to_swap [get_critical_cells]
-
-	if { $constraint == "hard"} {
-		#if the constraint is hard, swap before asking questions
-		set min_swap [expr ceil((1-$lvt)*[llength $cells_to_swap]) ]
-		set remaining_cells [lindex [swap_cells $cells_to_swap $min_swap] 1]
+	suppress_message PWR-246
+	suppress_message PWR-601
+	suppress_message PWR-602
+	
+	
+	set start_power [get_attribute [get_design] leakage_power]
+	if {$savings == 0} {
+		#Nothing to do
+		return
+	} elseif {$savings > 0 && $savings <= 0.2} { 
+		#Compute end power only
+		set end_power [expr $start_power*(1-$savings)]
+	} elseif {$savings > 0.2 && $savings < 1} {
+		#Compute end power and swap all cells into LVT
+                set end_power [expr $start_power*(1-$savings)]
+        	swap_all_lvt
 	} else {
-		set remaining_cells $cells_to_swap
+		#Just swap all cells into LVT
+		swap_all_lvt
 	}
 
-	#both the hard and soft implementations share this code
-	set X [expr ceil([llength $remaining_cells]/2.0)]
+	#Get a list of all cells in descending order of slack
+	set max_slack [get_max_slack]
 
-  	set slack [get_attribute [get_timing_paths] slack]
-	while { $slack > 0 && $X > 0} {
-		#we keep track of this to avoid recomputing the critical path when not needed
-		set old_slack $slack
-		#we try to swap X cells
-		set backtrack [swap_cells $remaining_cells $X]
-		set slack [get_attribute [get_timing_paths] slack]
-		if { $slack < 0 } {
-			#this means we swapped too many cells, so we unswap them back
-			set remaining_cells [lindex $backtrack 0]
-			unswap_cells $remaining_cells
-			lappend remaining_cells [lindex $backtrack 1]
-			#no need to recompute it, it's the same as before
-			set slack $old_slack
-		} else {
-			set remaining_cells [get_critical_cells]
-			#if {$X == 1} {set X 3.0}
+	#For each tuple in the list
+	foreach tuple $max_slack {
+		
+		#Get the cell name and reference
+		set cell_name [lindex $tuple 0]
+		set cell_ref [lindex $tuple 1]
+		
+		#If the current cell is of HVT type try to resize to its minimum possible
+		if { [get_attribute [get_lib_cell -of_object $cell_name] threshold_voltage_group] == "HVT"} {
+			#Get the cell current size
+			regexp {\d+$} $cell_ref size
+			#Get a list of alternative cells
+			foreach alt [alt_lib $cell_name $cell_ref] {
+				#Get the size of the alternative cell 
+				regexp {\d+$} $alt alt_size
+				#If the size retrieved from the library is smaller than the current one resize
+				if { $alt_size < $size } {
+					size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$alt
+					#If global slack is negative after resizing, undo
+					if { [get_attribute [get_timing_paths] slack] < 0} {
+						size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$cell_ref
+					#Otherwise cell was succesfully reduced to the minimum size and exit the loop
+					} else { 
+						break
+					}
+				#Cell cannot be reduced to less than current size
+				} else {
+					break
+				}
+			}
+		#If the current cell is of LVT type try to swap into HVT 
+		} elseif { [get_attribute [get_lib_cell -of_object $cell_name] threshold_voltage_group] == "LVT"} {
+			#Substitute the reference name from LVT to HVT before swapping
+			if { [regexp {CORE65LPLVT/HS65_LL} $cell_ref ] } {
+				regsub {CORE65LPLVT/HS65_LL} $cell_ref {CORE65LPHVT/HS65_LH} new_ref
+			} elseif { [regexp {CORE65LPLVT/HS65_LLS} $cell_ref ] } {
+                                regsub {CORE65LPLVT/HS65_LLS} $cell_ref {CORE65LPHVT/HS65_LHS} new_ref
+                        }
+			swap_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$new_ref
+			#If global slack is negative after swapping, undo and try to reduce size
+			if { [get_attribute [get_timing_paths] slack] < 0} {
+                        	swap_cell $cell_name CORE65LPLVT_nom_1.20V_25C.db:$cell_ref
+				regexp {\d+$} $cell_ref size
+                        	foreach alt [alt_lib $cell_name $cell_ref] {
+                               		regexp {\d+$} $alt alt_size
+                               		if { $alt_size < $size } {
+                                       		size_cell $cell_name CORE65LPLVT_nom_1.20V_25C.db:$alt
+                                       		if { [get_attribute [get_timing_paths] slack] < 0} {
+							size_cell $cell_name CORE65LPLVT_nom_1.20V_25C.db:$cell_ref
+                                       		} else {
+							break
+						}
+					} else {
+                                		break
+					}
+                        	}
+			#If slack is positive after swapping, try to reduce size 
+                        } else {
+				regexp {\d+$} $new_ref size
+	                        foreach alt [alt_lib $cell_name $new_ref] {
+        	                        regexp {\d+$} $alt alt_size
+                	                if { $alt_size < $size } {
+                        	                size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$alt
+                                	        if { [get_attribute [get_timing_paths] slack] < 0} {
+                                        	        size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$new_ref
+                                       		} else {
+							break
+						}
+                                	} else {
+                                		break
+					}	
+                        	}
+			}
 		}
-		if {$X == 1} {
-			#just to avoid entering an infinite loop
-			set X 0
-		} else {
-			#next iteration, we will try swapping half of the cells
-			set X [expr ceil($X/2.0)]
+		if { $savings < 1} {
+			if {[get_attribute [get_design] leakage_power -quiet] <= $end_power} {
+				break
+			}
 		}
 	}
-	return
+	
+	#In case the amount of savings required was not achieved in the first round that guarantees a positive slack
+	#starting swapping the cells with higher slacks into HVT (the critical cells are the last ones in the get_max_slack list)
+	
+	foreach tuple [get_max_slack] {
+
+                set cell_name [lindex $tuple 0]
+                set cell_ref [lindex $tuple 1]
+
+                #Substitute the reference name from LVT to HVT before swapping
+                if { [regexp {CORE65LPLVT/HS65_LL} $cell_ref ] } {
+                	regsub {CORE65LPLVT/HS65_LL} $cell_ref {CORE65LPHVT/HS65_LH} new_ref
+			swap_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$new_ref
+                } elseif { [regexp {CORE65LPLVT/HS65_LLS} $cell_ref ] } {
+                	regsub {CORE65LPLVT/HS65_LLS} $cell_ref {CORE65LPHVT/HS65_LHS} new_ref
+			swap_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$new_ref
+                } else {
+			set new_ref $cell_ref
+		}
+		#Get the current cell slack	
+		set cell_slack [get_attribute [get_timing_paths -nworst 1 -through $cell_name] slack]
+                regexp {\d+$} $new_ref size
+                foreach alt [alt_lib $cell_name $new_ref] {
+                	regexp {\d+$} $alt alt_size
+                        if { $alt_size < $size } {
+                        	size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$alt
+				#If the cell slack after swapping and resizing is lower than the current one, undo
+                                if {[get_attribute [get_timing_paths -nworst 1 -through $cell_name] slack] < $cell_slack} {
+                                	size_cell $cell_name CORE65LPHVT_nom_1.20V_25C.db:$new_ref
+                                } else {
+                                	break
+                                }
+                        } else {
+                        	break
+                        }
+		}
+		if { $savings < 1} {
+			if {[get_attribute [get_design] leakage_power -quiet] <= $end_power} {
+                        	break
+			}
+                }
+	}
+	#Following commands can be deleted for the final version (they are used for control only)
+	set saved [expr ($start_power - [get_attribute [get_design] leakage_power])/$start_power]
+	puts "Started: $start_power | Ended: [get_attribute [get_design] leakage_power] | Slack: [get_attribute [get_timing_paths] slack] | Saved: $saved"
 }
 
 define_proc_attributes dualVth \
 -info "Post-Synthesis Dual-Vth cell assignment" \
 -define_args \
-{
-	{-lvt "maximum % of LVT cells in range [0, 1]" lvt float required}
-	{-constraint "optimization effort: soft or hard" constraint one_of_string {required {values {soft hard}}}}
+{       
+        {-leakage "minimum % of leakage savings in range [0, 1]" value float required}
 }
 
-# this function sorts the all the output pins of the cells in the circuit, by decreasing slack
-proc get_critical_cells {} {
+proc get_max_slack {} {
 
-	set cells {}
-
-	#setting this variable to true is needed to get information about the slack of each pin
+	#Default variable from system that must be true
 	global timing_save_pin_arrival_and_slack
-	set timing_save_pin_arrival_and_slack 1
+        set timing_save_pin_arrival_and_slack 1
+	
+	set max_slack_list {}
+	
+	#Sort list from descending order of slack
+        foreach_in_collection pin [sort_collection [get_pins */Z] max_slack -descending] {
+                set cell [get_attribute $pin cell]
+                set ref_name [get_attribute $cell ref_name]
+		#Append the reference name ready to be used with swap_cell and size_cell
+		if { [regexp {_LL_|_LLS_} $ref_name ] } {
+			set ref_name "CORE65LPLVT/$ref_name"
+		}  elseif { [regexp {_LH_|_LHS_} $ref_name ] } {
+			set ref_name "CORE65LPHVT/$ref_name"
+		}
+		lappend max_slack_list "[get_attribute $cell full_name] $ref_name"
+        }
+	return $max_slack_list
+}
 
- 	#just this line allows us to have the pins already sorted
-	foreach_in_collection pin [sort_collection [get_pins */Z] max_slack -descending] {
-		set cell [get_attribute $pin cell]
-		set ref_name [get_attribute $cell ref_name]
-		if { [regexp {_LL} $ref_name ] } {
-			regsub {_LL} $ref_name {_LH} new_name
-			#we create a data structure that keeps every useful information, so no more manipulation is necessary
-			lappend cells [list [get_attribute $cell base_name] $ref_name $new_name]
+proc alt_lib {cell ref_name} {
+
+	#List containing the alternative cells for the input
+	set alternatives {}
+
+	regexp {.+\/(.+X)\d+} $ref_name match header
+	
+	#Get alternative cells for the same type but different sizes, i.e., a header equals to HS65_LH_NOR2X would only return
+	#alternative sizes of that cell
+	foreach_in_collection lib [get_alternative_lib_cells [get_cell $cell]] {
+		if { [regexp ".+$header.+" [get_attribute $lib full_name] ref] } {
+			lappend alternatives $ref
 		}
 	}
-	return $cells
+	set alternatives [lsort -dictionary $alternatives]
+	return $alternatives
+}
+
+proc swap_all_lvt {} {
+
+        #Sort list from descending order of slack
+        foreach_in_collection cell [get_cells] {
+                set ref_name [get_attribute $cell ref_name]
+                #Append the reference name ready to be used with swap_cell and size_cell
+                if { [regexp {HS65_LH} $ref_name ] } {
+                	regsub {HS65_LH} $ref_name {CORE65LPLVT/HS65_LL} ref_name
+                	swap_cell $cell CORE65LPLVT_nom_1.20V_25C.db:$ref_name
+		} elseif { [regexp {HS65_LHS} $ref_name ] } {
+                	regsub {HS65_LHS} $ref_name {CORE65LPLVT/HS65_LLS} ref_name
+			swap_cell $cell CORE65LPLVT_nom_1.20V_25C.db:$ref_name
+                }
+        }
 }
 
 
-proc swap_cells {cell_list num_swap} {
-	#set start_time [clock milliseconds]
-	set backtrack {}
-	set i 0
-	while { $i < $num_swap } {
-		set cell [lindex $cell_list 0]
-		lassign $cell name ref_name new_name
-		#a backtrack list is useful in case this swap was a mistake
-		lappend backtrack $cell
-		#swap the cell
-		size_cell $name $new_name
-		#remove the cell we just swapped from the list
-		set cell_list [lreplace $cell_list 0 0]
-		incr i
-	}
-	return [list $backtrack $cell_list]
-}
-
-
-# we call this function in case the last sizing made the slack negative
-proc unswap_cells {backtrack_list} {
-	foreach cell $backtrack_list {
-		size_cell [lindex $cell 0] [lindex $cell 1]
-	}
-}
